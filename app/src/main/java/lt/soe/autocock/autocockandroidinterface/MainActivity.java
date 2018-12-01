9e5fd3a64e87a7c628bc27d8b3059b3023044e18
abuicke <mosesjayjayjay@gmail.com>
2018-12-01 04:26:20 +0000

    updated package name

--- app/src/main/java/lt/soe/autocock/autocockandroidinterface/MainActivity.java
+++ app/src/main/java/lt/soe/autocock/autocockandroidinterface/MainActivity.java
@@ -1,7 +1,9 @@
-package lt.soe.autocock;
+package lt.soe.autocock.autocockandroidinterface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import lt.soe.autocock.R;
 
 public class MainActivity extends AppCompatActivity {
 
