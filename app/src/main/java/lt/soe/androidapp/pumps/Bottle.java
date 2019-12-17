7b434ef16b89ed55f04884bf93b06c50ce5d94f7
abuicke <mosesjayjayjay@gmail.com>
2019-12-17 17:40:00 +0000

    reloved unncessary contructors from Pump and Bottle

--- app/src/main/java/lt/soe/androidapp/pumps/Bottle.java
+++ app/src/main/java/lt/soe/androidapp/pumps/Bottle.java
@@ -6,16 +6,6 @@ public class Bottle {
     public int fullBottleVolumeMillilitres;
     public int currentVolumeMillilitres;
 
-    public Bottle(String name, int fullBottleVolumeMillilitres, int currentVolumeMillilitres) {
-        this.name = name;
-        this.fullBottleVolumeMillilitres = fullBottleVolumeMillilitres;
-        this.currentVolumeMillilitres = currentVolumeMillilitres;
-    }
-
-    public Bottle() {
-
-    }
-
     @Override
     public String toString() {
         return "Bottle{" +
--- app/src/main/java/lt/soe/androidapp/pumps/Pump.java
+++ app/src/main/java/lt/soe/androidapp/pumps/Pump.java
@@ -4,14 +4,6 @@ public class Pump {
 
     public Bottle bottle;
 
-    public Pump(Bottle derp) {
-        bottle = derp;
-    }
-
-    public Pump() {
-
-    }
-
     @Override
     public String toString() {
         return bottle.toString();
